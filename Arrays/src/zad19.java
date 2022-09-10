65286fb8fc7bc13b429b796470ab3909e2f80c53
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-09-10 23:32:29 +0300

    solution

---
+++ Arrays/src/zad19.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class zad19 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int[] nums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).toArray();
+		int length = 1;
+		int max = 0;
+		int index = 0;
+
+		for (int i = 0; i < nums.length - 1; i++) {
+
+			if (nums[i] < nums[i + 1]) {
+				length++;
+
+			} else {
+				if (length > max) {
+					max = length;
+					index = i;
+					length = 1;
+				}
+			}
+
+		}
+		int[] result = Arrays.copyOfRange(nums, index - (max - 1), index + 1);
+		for (int i : result) {
+			System.out.println(i + " ");
+		}
+
+	}
+
+}

99a2ef923f501534cce93c8c8e2ff7fdd9308aaf
kalin73 <104739268+kalin73@users.noreply.github.com>
2022-10-30 23:34:19 +0200

    solution

--- Maps_Lambda_Streams/src/Snowwhite.java
+++ Maps_Lambda_Streams/src/Snowwhite.java
@@ -1,24 +1,15 @@
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-//NOT FINISHED!!!
 public class Snowwhite {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 
-		LinkedHashMap<String, Dwarf> map = new LinkedHashMap<>();
-		List<String> list = new ArrayList<>();
+		LinkedHashMap<String, LinkedHashMap<String, Integer>> hats = new LinkedHashMap<>();
+		LinkedHashMap<String, Integer> dwarfs = new LinkedHashMap<>();
 		String[] dwarf = input.split("<:>");
 
 		while (!input.equals("Once upon a time")) {
@@ -26,126 +17,47 @@ public class Snowwhite {
 			String name = dwarf[0].trim();
 			String hatColor = dwarf[1].trim();
 			int physics = Integer.parseInt(dwarf[2].trim());
-			Dwarf dw = map.get(hatColor);
 
-			if (!map.containsKey(hatColor)) {
-				map.put(hatColor, new Dwarf(name, physics, 1));
+			if (!hats.containsKey(hatColor)) {
+				dwarfs = new LinkedHashMap<>();
+				dwarfs.put(name, physics);
+				hats.put(hatColor, dwarfs);
 				input = sc.nextLine();
 				continue;
 
-			} else if (map.get(hatColor).getName().equals(name)) {
-				dw.setPhysics(Math.max(physics, map.get(hatColor).getPhysics()));
-				map.put(hatColor, dw);
+			} else if (hats.get(hatColor).containsKey(name)) {
+				dwarfs = hats.get(hatColor);
+				dwarfs.put(name, Math.max(dwarfs.get(name), physics));
+				hats.put(hatColor, dwarfs);
 				input = sc.nextLine();
 				continue;
 
 			}
-
-			map.put(hatColor, new Dwarf(name, physics, 1));
+			dwarfs = hats.get(hatColor);
+			dwarfs.put(name, physics);
+			hats.put(hatColor, dwarfs);
 			input = sc.nextLine();
 
 		}
-		Collections.sort(list, (s, f) -> s.compareTo(f));
-////map.entrySet().stream().sorted((f,s)->s.getValue().g)
-//		map.entrySet().stream().sorted((f, s) -> Integer.compare(s.getKey().getHatCount(), f.getKey().getHatCount()))
-//				.sorted((fs, se) -> Integer.compare(se.getValue().getPhysics(), fs.getValue().getPhysics()))
-//				.forEach(x -> System.out.println(x.getKey().getHatColor() + " " + x.getValue().getName() + " "
-//						+ x.getValue().getPhysics() + " " + x.getKey().getHatCount()));
 
-//		Collections.sort(dwarfs, (x1, x2) -> Integer.compare(x2.getPhysics(), x1.getPhysics()));
-//		dwarfs.forEach((x) -> {
-//
-//			System.out.println(x.getName() + " " + x.getPhysics());
-//
-//		});
+		Map<String, Integer> hatSorted = new LinkedHashMap<>();
+		hats.entrySet()
+		        .stream()
+		        .sorted((f, s) -> Integer.compare(s.getValue().size(), f.getValue().size()))
+				.forEach(x -> {
+					x.getValue().forEach((k, v) -> {
+						hatSorted.put(x.getKey() + " " + k, v);});
+
+				});
+		
+		hatSorted.entrySet()
+		         .stream()
+		         .sorted((f, s) -> Integer.compare(s.getValue(), f.getValue()))
+		         .forEach(x -> {
+			              String[] arr = x.getKey().split(" ");
+			              System.out.printf("(%s) %s <-> %d%n", arr[0], arr[1], x.getValue());
+		});
+		sc.close();
+		
 	}
-}
-
-class Dwarf {
-	private String name;
-	private int physics;
-	int counter;
-
-	public Dwarf() {
-
-	}
-
-	public Dwarf(String name, int physics, int counter) {
-		this.name = name;
-		this.physics = physics;
-	}
-
-	public Dwarf(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public int getPhysics() {
-		return physics;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setPhysics(int physics) {
-		this.physics = physics;
-	}
-
-	public int getCounter() {
-		return counter;
-	}
-}
-
-class HatCounter {
-
-	String hatColor;
-	int hatCount;
-
-	public HatCounter(String hatColor, int hatCount) {
-
-		this.hatColor = hatColor;
-		this.hatCount = hatCount;
-	}
-
-	public HatCounter() {
-
-	}
-
-	public String getHatColor() {
-		return hatColor;
-	}
-
-	public void setHatColor(String hatColor) {
-		this.hatColor = hatColor;
-	}
-
-	public int getHatCount() {
-		return hatCount;
-	}
-
-	public void setHatCount(int hatCount) {
-		this.hatCount = hatCount;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(hatColor);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		HatCounter other = (HatCounter) obj;
-		return Objects.equals(hatColor, other.hatColor);
-	}
-
 }

d7d74173da93c5afb2173c3f31670ea2ca279331
kalin73 <104739268+kalin73@users.noreply.github.com>
2023-07-19 01:54:58 +0300

    solution

--- Arrays/src/zad19.java
+++ Arrays/src/zad19.java
@@ -1,34 +1,55 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class zad19 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int[] arr = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
-		int[] nums = Arrays.stream(sc.nextLine().split(" ")).mapToInt(x -> Integer.parseInt(x)).toArray();
-		int length = 1;
-		int max = 0;
-		int index = 0;
-		for (int i = 0; i < nums.length - 1; i++) {
+		int[] prev = new int[arr.length];
+		int[] length = new int[arr.length];
 
-			if (nums[i] < nums[i + 1]) {
-				length++;
+		Arrays.fill(prev, -1);
 
-			} else {
-				if (length > max) {
-					max = length;
-					index = i;
-					length = 1;
+		int maxLength = 0, maxIndex = -1;
+
+		for (int i = 0; i < arr.length; i++) {
+			int current = arr[i];
+			int bestLength = 1;
+			int bestIndex = -1;
+
+			for (int j = i - 1; j >= 0; j--) {
+				if (arr[j] < current && length[j] + 1 >= bestLength) {
+					bestLength = length[j] + 1;
+					bestIndex = j;
+
 				}
 			}
+			prev[i] = bestIndex;
+			length[i] = bestLength;
+
+			if (maxLength < bestLength) {
+				maxLength = bestLength;
+				maxIndex = i;
+			}
+		}
+		List<Integer> lis = new ArrayList<>();
+
+		int index = maxIndex;
+
+		while (index != -1) {
+			lis.add(arr[index]);
+			index = prev[index];
+		}
+
+		for (int i = lis.size() - 1; i >= 0; i--) {
+			System.out.print(lis.get(i) + " ");
 
 		}
-		int[] result = Arrays.copyOfRange(nums, index - (max - 1), index + 1);
-		for (int i : result) {
-			System.out.println(i + " ");
-		}
+
 		sc.close();
 	}
-
 }

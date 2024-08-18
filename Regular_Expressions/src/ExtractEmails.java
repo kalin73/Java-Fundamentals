c98743746b1fd4897dd100387a5d47bd254c310d
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-18 12:36:18 +0300

    solution

--- Regular_Expressions/src/ExtractEmails.java
+++ Regular_Expressions/src/ExtractEmails.java
@@ -1,8 +1,26 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExtractEmails {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		String input = sc.nextLine();
+
+		final String user = "[A-Za-z0-9]+[-\\._]?[A-Za-z0-9]+";
+		final String host = "[A-Za-z]+\\-?[A-Za-z]+(\\.[A-Za-z]+\\-?[A-Za-z]+)+";
+		final String regex = user + "@" + host;
+
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(input);
+
+		while (matcher.find()) {
+			System.out.println(matcher.group());
+		}
+
+		sc.close();
 
 	}
 

d2e29f32a747f92c07ad1510ce3a9c3efae3d7a1
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-02-01 21:05:21 +0200

    refactoring solution

--- Regular_Expressions/src/MatchPhoneNumber.java
+++ Regular_Expressions/src/MatchPhoneNumber.java
@@ -10,16 +10,16 @@ public class MatchPhoneNumber {
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 
-		String regex = "\\+359([- ])2\\1[\\d]{3}\\1[\\d]{4}\\b";
+		String regex = "\\+359([- ])2\\1\\d{3}\\1\\d{4}\\b";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(input);
-		
+
 		List<String> phoneNumbers = new ArrayList<>();
 
 		while (matcher.find()) {
 			phoneNumbers.add(matcher.group());
 		}
-		
+
 		System.out.println(String.join(", ", phoneNumbers));
 		sc.close();
 

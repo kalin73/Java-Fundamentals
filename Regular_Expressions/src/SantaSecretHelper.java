701f46998f985148a1810bdd37c3da6751261820
kalin73 <104739268+kalin73@users.noreply.github.com>
2025-03-10 19:35:37 +0200

    solution

---
+++ Regular_Expressions/src/SantaSecretHelper.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SantaSecretHelper {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        String line = "";
+        Pattern pattern = Pattern.compile("@([A-Za-z]+)[^@\\-!:>]+!([GN])!");
+
+        while (!(line = sc.nextLine()).equals("end")) {
+            StringBuilder decrypted = new StringBuilder();
+            for (char c : line.toCharArray()) {
+                decrypted.append((char) (c - n));
+            }
+            Matcher matcher = pattern.matcher(decrypted);
+
+            if (matcher.find()) {
+                if (matcher.group(2).equals("G")) {
+                    System.out.println(matcher.group(1));
+                }
+            }
+        }
+        
+        sc.close();
+    }
+}
